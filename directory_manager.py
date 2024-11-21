b346ecc8d2c7dd642d77a3b830527babe3e4e7ac
Max Herbert <w0494420@campus.nscc.ca>
2024-11-21 17:32:22 -0400

    FMDA-4

--- directory_manager.py
+++ directory_manager.py
@@ -19,6 +19,10 @@ class FileMoverApp(QMainWindow):
         self.target_dir_button.clicked.connect(self.select_target_directory)
         self.move_button.clicked.connect(self.move_selected_files)
 
+        self.source_sort_dropdown.currentIndexChanged.connect(lambda: self.sort_files(self.source_file_list, self.source_dir_edit.text(), self.source_sort_dropdown.currentIndex()))
+        self.target_sort_dropdown.currentIndexChanged.connect(lambda: self.sort_files(self.target_file_list, self.target_dir_edit.text(), self.target_sort_dropdown.currentIndex()))
+
+
     def select_source_directory(self):
         source_directory = QFileDialog.getExistingDirectory(self, "Select Source Directory")
         if source_directory:
@@ -69,3 +73,30 @@ class FileMoverApp(QMainWindow):
 
         self.load_files(source_directory, self.source_file_list)
         self.load_files(target_directory, self.target_file_list)
+
+    def sort_files(self, list_widget, directory, sort_option):
+        """
+        Sort files in the given list widget based on the selected sort option.
+        """
+        if not directory or not os.path.exists(directory):
+            return
+
+        files = os.listdir(directory)
+
+        if sort_option == 0:  # Sort by File Type (Ascending)
+            files.sort(key=lambda x: os.path.splitext(x)[1])
+        elif sort_option == 1:  # Sort by File Type (Descending)
+            files.sort(key=lambda x: os.path.splitext(x)[1], reverse=True)
+        elif sort_option == 2:  # Sort Alphabetically (Ascending)
+            files.sort()
+        elif sort_option == 3:  # Sort Alphabetically (Descending)
+            files.sort(reverse=True)
+        elif sort_option == 4:  # Sort by Last Modified Date (Ascending)
+            files.sort(key=lambda x: os.path.getmtime(os.path.join(directory, x)))
+        elif sort_option == 5:  # Sort by Last Modified Date (Descending)
+            files.sort(key=lambda x: os.path.getmtime(os.path.join(directory, x)), reverse=True)
+
+        # Update the list widget with the sorted files
+        list_widget.clear()
+        for file in files:
+            list_widget.addItem(file)
